b20db13c613b3c19a53e347eafaaf41165f29492
Martin Joubert <martin.joubert@teampicnic.com>
2022-01-10 10:03:05 +0100

    Add comments in index.js

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ yarn install
 3. Navigate to `/ios` and run `pod install`.
 
 4. Use `yarn ios` or `yarn android` to run on the perspective platforms.
+
+## Preview
+
+![homescreen](https://raw.githubusercontent.com/martinjbrt/swipeable/master/assets/android.mov)
+![login](https://raw.githubusercontent.com/martinjbrt/swipeable/master/assets/ios.mov)
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// We need to add react-native-gesture-handler as the first import for React Native 0.61 or greater
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
+// We need to wrap our entry point in gestureHandlerRootHOC for Android to work
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
